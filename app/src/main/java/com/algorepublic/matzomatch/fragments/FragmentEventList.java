f6d4993fd6bb67981a2c057af9820b7d8f13276f
ahmadmiraj <ahmadmiraj@outlook.com>
2015-12-03 19:58:43 +0500

    latest code
    Signed-off-by: ahmadmiraj <ahmadmiraj@outlook.com>

--- app/src/main/java/com/algorepublic/matzomatch/fragments/FragmentEventList.java
+++ app/src/main/java/com/algorepublic/matzomatch/fragments/FragmentEventList.java
@@ -24,6 +24,7 @@ public class FragmentEventList extends Fragment {
     View view;
     AQuery aq;
     EventService eventService;
+    Arra
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
